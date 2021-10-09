cfb31ceab9a43f17df26b29832fa186413151f91
vadim2071 <vadim207138@gmail.com>
2021-10-09 19:36:35 +0300

    Lesson02 - part01

--- Lesson_02/ClassLibrary/BankAccount.cs
+++ Lesson_02/ClassLibrary/BankAccount.cs
@@ -1,17 +1,49 @@
 ﻿using System;
 
+
 namespace ClassLibrary
 {
+    public enum typeAccount { debit, kredit }
     public class BankAccount
     {
-        int AccountNum;
-        int AccountBalance;
-        enum AccountType
+        private int accountNum;
+        private int accountBalance;
+        private typeAccount accountType;
+
+        public int AccountNum //свойство - номер счета
         {
-            debit,
-            kredit
+            get
+            {
+                return this.accountNum;
+            }
+            set
+            {
+                this.accountNum = value;
+            }
+        }
+        public int AccountBalance //свойство - баланс счета
+        {
+            get
+            {
+                return this.accountBalance;
+            }
+            set
+            {
+                this.accountBalance = value;
+            }
         }
 
+        public typeAccount AccountType //свойство - тип счета
+        {
+            get
+            {
+                return this.accountType;
+            }
+            set
+            {
+                this.accountType = value;
+            }
+        }
 
 
     }
--- Lesson_02/Lesson_02/Program.cs
+++ Lesson_02/Lesson_02/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using ClassLibrary;
+//1.Создать класс счет в банке с закрытыми полями: номер счета, баланс, тип банковского счета (использовать перечислимый тип).
+//  Предусмотреть методы для доступа к данным – заполнения и чтения.
+//  Создать объект класса, заполнить его поля и вывести информацию об объекте класса на печать.
 
 namespace Lesson_02
 {
@@ -7,7 +10,17 @@ namespace Lesson_02
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BankAccount myAccaunt = new BankAccount();
+            myAccaunt.AccountNum = 1;
+            myAccaunt.AccountBalance = 1000;
+            myAccaunt.AccountType = typeAccount.debit;
+            Console.WriteLine("Мой новый счет! \n" +
+                              "номер счета  - {0}\n" +
+                              "Баланс счета - {1}\n" +
+                              "Тип счета    - {2}",
+                              myAccaunt.AccountNum,
+                              myAccaunt.AccountBalance,
+                              myAccaunt.AccountType);
         }
     }
 }
