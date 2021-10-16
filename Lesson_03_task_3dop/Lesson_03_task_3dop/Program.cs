040a38effaa1c5cc0ee9827a8b7664c776d3b17a
vadim2071 <vadim207138@gmail.com>
2021-10-17 00:36:33 +0300

    Lesson02 - end

--- Lesson_03_task_3dop/Lesson_03_task_3dop/Program.cs
+++ Lesson_03_task_3dop/Lesson_03_task_3dop/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.IO;
 
 //3. * Работа со строками.
@@ -22,7 +24,13 @@ namespace Lesson_03_task_3dop
         static void Main(string[] args)
         {
             //массив для тестов
-            string[] testArray = { "Кучма Андрей Витальевич & Kuchma@mail.ru", "Мизинцев Павел Николаевич & Pasha@mail.ru  ", "Иванов Иван Иванович &    ivan@.mail.ru", "Петров Петр Петрович & p.petrov@mail.ru"};
+            string[] testArray = {  "Кучма Андрей Витальевич &Kuchma@mail.ru", 
+                                    "Мизинцев Павел Николаевич & Pasha@mail.ru  ", 
+                                    "Иванов Иван Иванович &    ivan@.mail.ru", 
+                                    "Петров Петр Петрович & p.petrov@mail.ru",
+                                    "Сидоров Иван Петрови&i.sidorovmail.ru",
+                                    "Феофанов Авросий Батькович&  a.feofanov@mailru",
+                                    "Гаврилов Гаврил Семенович & g.gavrilov@u.r"};
             string FilePass = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\"; // путь для сохранения файла
             string filename = "test.txt"; //имя файла для записи
             File.WriteAllText(FilePass + filename, testArray[0]); // создаем / очищаем существующий файл для записи дерева
@@ -31,13 +39,50 @@ namespace Lesson_03_task_3dop
                 File.AppendAllText(FilePass + filename, Environment.NewLine);
                 File.AppendAllText(FilePass + filename, testArray[i]);
             }
-            //вывод списка из файла
-            while (File(FilePass + filename))
 
+            //вывод списка электронной почты из файла
+            StreamReader _file = new StreamReader(FilePass + filename);
+            string _string;
+            while (!_file.EndOfStream)
+            {
+                _string = _file.ReadLine();
+                Console.WriteLine(_string);
+                SearchMail(ref _string);
+                Console.WriteLine("Адрес электронной почты - {0}\n", _string);
+            }
 
             static void SearchMail (ref string s)
             {
+                int _index;
+                string _string;
+                _index = s.IndexOf("&"); //ищем позицию разделитель
+                s = s.Substring(_index + 1);
 
+                //проверка на наличие лишних пробелов после разделителя и удаление их
+                while (s.IndexOf(" ") == 0)
+                {
+                    s = s.Substring(1);
+                }
+                //простая проверка на соответствие строки электронной почте. Проверяю на наличие знаков @ и .
+
+                _index = s.IndexOf("@");
+                // Если @ написан в начале строки или после него 3 символа (не хватит на имя домена и имя зоны) или нет символа @
+                if ((_index <= 0) || (_index >= s.Length-4) )
+                {
+                    s = "Ошибка в написании адреса электроной почты - " + s;
+                    return;
+                }
+                _string = s.Substring(_index + 1);
+
+                //Теперь проверяем положении точки, так как у на супрщенная проверка не учитываем то что в адресе может быть имя домена и поддомена
+                _index = _string.IndexOf(".");
+
+                //если точка стоит сразу после @ или после точки один символ или точки совсем нет
+                if ((_index <= 0) || (_index >= _string.Length - 1))
+                {
+                    s = "Ошибка в написании адреса электроной почты - " + s;
+                    return;
+                }
             }
         }
     }
