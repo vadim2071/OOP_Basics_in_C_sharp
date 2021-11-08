3510532b5a866ace26ebf0a4dbaaf8cb55cb0242
vadim2071 <vadim207138@gmail.com>
2021-11-09 00:10:50 +0300

    Lesson 05 +

---
+++ Lesson_05/Lesson05/ClassLibrary/RationalNum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class RationalNum //класс - рациональной число
+    {
+        private int _numerator; //числитель
+        private int _denominator; //знаменатель
+    }
+
+    public RationalNum(int numerator, int denominator)
+    {
+
+    }
+}
