577bd8637ae7a07fb453b2c66ce1a1192ff4d566
vadim2071 <vadim207138@gmail.com>
2021-11-09 22:00:59 +0300

    Lesson05++

--- Lesson_05/Lesson05/ClassLibrary/RationalNum.cs
+++ Lesson_05/Lesson05/ClassLibrary/RationalNum.cs
@@ -6,10 +6,28 @@ namespace ClassLibrary
     {
         private int _numerator; //числитель
         private int _denominator; //знаменатель
+
+        public int Numerator
+        {
+            get { return _numerator; }
+        }
+        public int Denominator
+        {
+            get { return _denominator; }
+        }
+
+        public RationalNum(int numerator, int denominator)
+        {
+            if (denominator <= 0) throw new ArgumentException("denominator"); //на 0 делить нельзя и отрицательным пусть может быть только числитель
+            _numerator = numerator;
+            _denominator = denominator;
+        }
+
+        public static RationalNum operator +(RationalNum rN1, RationalNum rN2)
+        {
+            return new RationalNum(rN1._numerator + rN2._numerator, rN1.Denominator + rN2.Denominator);
+        }
+
     }
 
-    public RationalNum(int numerator, int denominator)
-    {
-
-    }
 }
