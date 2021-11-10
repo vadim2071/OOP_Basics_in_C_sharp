eacaae8e2914cbb844394da67bfd40240a84be53
Vadim2071 <vadim207138@gmail.com>
2021-11-10 17:09:24 +0300

    Lesson05+++

--- Lesson_05/Lesson05/ClassLibrary/RationalNum.cs
+++ Lesson_05/Lesson05/ClassLibrary/RationalNum.cs
@@ -30,21 +30,99 @@ namespace ClassLibrary
 
         public static RationalNum operator +(RationalNum rNum1, RationalNum rNum2) // оператор сложения
         {
-            //int a1 = rNum1._numerator;  // числитель 1го числа
             int b1 = rNum1.Denominator; // знаменатель 1го числа
-            //int a2 = rNum2._numerator;  // числитель 2го числа
             int b2 = rNum2.Denominator; // знаменатель 2го числа
 
             //приводим к общему знаменателю и складываем
-
             return new RationalNum((rNum1._numerator * b2) + (rNum2._numerator * b1), b2 * b1);
         }
+        public static RationalNum operator -(RationalNum rNum1, RationalNum rNum2) // оператор вычетания
+        {
+            int b1 = rNum1.Denominator; // знаменатель 1го числа
+            int b2 = rNum2.Denominator; // знаменатель 2го числа
 
+            //приводим к общему знаменателю и вычитаем
+            return new RationalNum((rNum1._numerator * b2) - (rNum2._numerator * b1), b2 * b1);
+        }
+
+        public static RationalNum operator *(RationalNum rNum1, RationalNum rNum2) // оператор умножения
+        {
+            return new RationalNum(rNum1._numerator * rNum2._numerator , rNum1._denominator * rNum2._denominator);
+        }
+
+        public static RationalNum operator /(RationalNum rNum1, RationalNum rNum2) // оператор деления
+        {
+            return new RationalNum(rNum1._numerator * rNum2._denominator, rNum1._denominator * rNum2._numerator);
+        }
+
+        public static int operator %(RationalNum rNum1, RationalNum rNum2) // оператор остаток от деления -------------------------------------------------------
+        {
+            RationalNum resultDel = new RationalNum(rNum1._numerator * rNum2._denominator, rNum1._denominator * rNum2._numerator); //делим
+            resultDel = resultDel - rNum1; //из результат деления вычитаем
+            int ostatok = (int)resultDel;
+            return ostatok;
+        }
+
+        public static RationalNum operator ++(RationalNum rNum) //оператор инкремент ++
+        {
+            return new RationalNum(rNum._numerator + rNum._denominator, rNum._denominator);
+        }
+
+        public static RationalNum operator --(RationalNum rNum) //оператор декремент --
+        {
+            return new RationalNum(rNum._numerator - rNum._denominator, rNum._denominator);
+        }
+
+        public static bool operator ==(RationalNum rNum1, RationalNum rNum2) //оператор сравнения ==
+        {
+            //приводим к общему знаменателю и сравниваем
+            //знаменатели сравнивать нет необходимости, они равны
+            return ((rNum1._numerator * rNum2._denominator) == (rNum2._numerator * rNum1._denominator));
+        }
+
+        public static bool operator !=(RationalNum rNum1, RationalNum rNum2) //оператор сравнения !=
+        {
+            //приводим к общему знаменателю и сравниваем
+            //знаменатели сравнивать нет необходимости, они равны
+            return ((rNum1._numerator * rNum2._denominator) != (rNum2._numerator * rNum1._denominator));
+        }
+
+        public static bool operator >(RationalNum rNum1, RationalNum rNum2) //оператор сравнения >
+        {
+            return (rNum1._numerator * rNum2._denominator > rNum2._numerator * rNum1._denominator);
+        }
+
+        public static bool operator <(RationalNum rNum1, RationalNum rNum2) //оператор сравнения <
+        {
+            return (rNum1._numerator * rNum2._denominator < rNum2._numerator * rNum1._denominator);
+        }
+
+        public static bool operator >=(RationalNum rNum1, RationalNum rNum2) //оператор сравнения >=
+        {
+            return (rNum1._numerator * rNum2._denominator >= rNum2._numerator * rNum1._denominator);
+        }
+
+        public static bool operator <=(RationalNum rNum1, RationalNum rNum2) //оператор сравнения <=
+        {
+            return (rNum1._numerator * rNum2._denominator <= rNum2._numerator * rNum1._denominator);
+        }
+
+        public static implicit operator string(RationalNum rNum) //Преобразование в String
+        {
+            string rnum = rNum._numerator.ToString() + "/" + rNum._denominator.ToString();
+            return (rnum);
+        }
+
+        public static implicit operator float(RationalNum rNum) //Преобразование в float
+        {
+            float _numerator = (float)rNum._numerator;
+            float _denominator = (float)rNum._denominator;
+            float rnum = _numerator / _denominator;
+            return (rnum);
+        }
+        public static implicit operator int(RationalNum rNum) //Преобразование в int
+        {
+           return (rNum._numerator / rNum._denominator);
+        }
     }
-
-    public static void PrintRationalNum(RationalNum _num1, RationalNum _num2)
-    {
-        Console.WriteLine("{0} / {1}", this.Numerator, this.Denominator);
-    }
-
 }
--- Lesson_05/Lesson05/Lesson05/Program.cs
+++ Lesson_05/Lesson05/Lesson05/Program.cs
@@ -20,10 +20,44 @@ namespace Lesson05
             RationalNum Num1 = new RationalNum(1, 5);
             RationalNum Num2 = new RationalNum(5, 8);
             RationalNum Result;
+            Console.WriteLine("Рациаональное число 1");
             Num1.Print();
+            Console.WriteLine("Рациаональное число 2");
             Num2.Print();
+            Console.WriteLine("Сумма двух рациональных чисел");
             Result = Num1 + Num2;
             Result.Print();
+            Console.WriteLine("Разность двух рациональных чисел");
+            Result = Num1 - Num2;
+            Result.Print();
+            Console.WriteLine("Инкремент рационального числа");
+            Num1.Print();
+            Num1++;
+            Console.WriteLine("Результат");
+            Num1.Print();
+            Console.WriteLine("Декремент рационального числа");
+            Num1.Print();
+            Num1--;
+            Console.WriteLine("Результат");
+            Num1.Print();
+
+            Console.WriteLine("Сравнения");
+            Console.WriteLine("1/5 == 5/8 {0} ", Num1 == Num2);
+            Console.WriteLine("1/5 != 5/8 {0} ", Num1 != Num2);
+            Console.WriteLine("1/5 > 5/8 {0} ", Num1 > Num2);
+            Console.WriteLine("1/5 < 5/8 {0} ", Num1 < Num2);
+            Console.WriteLine("1/5 >= 5/8 {0} ", Num1 >= Num2);
+            Console.WriteLine("1/5 <= 5/8 {0} ", Num1 <= Num2);
+
+            string rNumToString = (string)Num1;
+            Console.WriteLine("Преобразование в String - " + rNumToString);
+
+            float rNumToFloat = (float)Num1;
+            Console.WriteLine("Преобразование в Float - " + rNumToFloat);
+
+            int rNumToInt = (int)Num1;
+            Console.WriteLine("Преобразование в Int - " + rNumToInt);
+
         }
 
     }
