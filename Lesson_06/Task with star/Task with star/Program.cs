5e59042142610c031bdbbad0bd5628b1f1662abc
IVG <vadim207138@gmail.com>
2021-11-16 01:29:42 +0300

    Lesson06 Ttask with star Begin

---
+++ Lesson_06/Task with star/Task with star/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task_with_star
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
