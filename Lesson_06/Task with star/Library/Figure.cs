e40246581999a380b71ac5a4168edd463c3c040f
vadim2071 <vadim207138@gmail.com>
2021-11-18 23:07:52 +0300

    Lesson06 Task star++

--- Lesson_06/Task with star/Library/Figure.cs
+++ Lesson_06/Task with star/Library/Figure.cs
@@ -10,19 +10,39 @@
 
 namespace Library
 {
-    enum color { red, green, blue, white, black }
+    public enum color { red, green, blue, white, black }
     public class Figure
     {
+        //public enum color { red, green, blue, white, black }
         private color _color; // цвет фигуры
         private bool _visible; // состояние видимости
 
-        public color Color { get { return this._color; } set { this._color = value; } };
-        public bool Visible { get { return this._visible; } set { this._visible = value; } };
+        public color ColorFig { get { return this._color; } set { this._color = value; } }
+        public bool VisibleFig { get { return this._visible; } set { this._visible = value; } }
 
-        public Figure (color Color, bool Visible)
+        public Figure(color color, bool visible)
         {
-            _color = Color;
-            _visible = Visible;
+            _color = color;
+            _visible = visible;
+        }
+        public bool Visible()
+        {
+            return this._visible;
+        }
+        public void ChangeColorTo (color color)
+        {
+            this._color = color;
+        }
+        public abstract void MoveHorizont(int move);
+
+        public void MoveVertikal(int move)
+        {
+
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine("Цвет фигуры " + this._color);
+            Console.WriteLine("Видимость фигуры " + this._visible);
         }
     }
 }
---
+++ Lesson_06/Task with star/Library/Point.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Point : Figure
+    {
+        public Point(color color, bool visible) : base(color, visible)
+        {
+        }
+        private int _x; // координаты x
+        private int _y; // координата y
+
+        public override void MoveHorizont(int move)
+        {
+
+        }
+    }
+}
