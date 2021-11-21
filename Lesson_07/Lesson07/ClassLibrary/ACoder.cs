d1caf710147627a00098df411ca988f392e56b50
IVG <vadim207138@gmail.com>
2021-11-21 22:44:17 +0300

    Lesson07 Begin

---
+++ Lesson_07/Lesson07/ClassLibrary/ACoder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class ACoder : ICOder
+    {
+        public string Encoder (string _in)
+        {
+            return _in;
+        }
+        public string Decoder (string _in)
+        {
+            return _in;
+        }
+    }
+}
---
+++ Lesson_07/Lesson07/ClassLibrary/ICOder.cs
@@ -0,0 +1,13 @@
+﻿
+
+
+using System;
+
+namespace ClassLibrary
+{
+    interface ICOder
+    {
+        string Encode(string _in);
+        string Decode(string _in);
+    }
+}
