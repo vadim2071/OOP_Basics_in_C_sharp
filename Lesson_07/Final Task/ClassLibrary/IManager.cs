0bded555b8a3ef3fb49be7e96b270583a4e61ab8
vadim2071 <vadim207138@gmail.com>
2021-12-07 23:21:17 +0300

    FinalTask Begin

---
+++ Lesson_07/Final Task/ClassLibrary/IManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    internal interface IManager
+    {
+        public void Rename(string newName);
+        public void Delete();
+        public void MakeNew(string newName);
+        public void Copy(string newName);
+        public void Move(string newName);
+        public float GetSize();
+        public void Search(string search);
+        public void ChangeAttribute(string attribite, string newAttribute);
+
+    }
+}
---
+++ Lesson_07/Final Task/Final Task/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Final_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
