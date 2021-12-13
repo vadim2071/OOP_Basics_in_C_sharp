201423ec5a8760a0dea70724a0563ecd72ab69cf
Vadim <vadim207138@gmail.com>
2021-12-13 20:49:43 +0300

    FinalTask++

---
+++ Final Task/ClassLibrary/FMBaseClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public abstract class FMBaseClass
+    {
+        private string _curentPass; //текущий маршрут до объекта класса
+
+        public string CurentPass { get { return _curentPass; },set { _curentPass = value; } }
+
+        public virtual void Rename(string newName)
+        {
+
+        }
+        public void Delete()
+        {
+
+        }
+        public void MakeNew(string newName)
+        {
+
+        }
+        public void Copy(string newName)
+        {
+
+        }
+        public void Move(string newName)
+        {
+
+        }
+        public float GetSize()
+        {
+            return 0;
+        }
+        public void Search(string search)
+        {
+
+        }
+        public void ChangeAttribute(string attribite, string newAttribute)
+        {
+
+        }
+    }
+}
--- Final Task/ClassLibrary/FMfile.cs
+++ Final Task/ClassLibrary/FMfile.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ClassLibrary
 {
-    internal class File
+    public class FMfile : FMBaseClass
     {
     }
 }
